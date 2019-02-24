a9b363459b16c433ee553ce7e46ecdb90dd2e297
Dima Dubovski <dzmitry.duboyski@gmail.com>
2019-02-24 21:52:50 +0200

    upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include("includes.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Поиск</title>
+</head>
+<body>
+<h1>Поиск пациентов находившихся на лечении  в больнице</h1>
+<form action="find.php" method="post">
+	Введите данные о пациенте
+	<br>
+	<br>
+	Фамилия Имя Отчество:
+	<input type="text" name="fio" value="">
+	<br>
+	<!-- Период поиска, с 2004 по 2014 -->
+	<input type="submit" value="Поиск" name="submit">
+	<br>
+</form>
+</body>
+</html>
